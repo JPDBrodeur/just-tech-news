d61bb3637226a0ce2b70a278f705e747f7fc238f
JPDBrodeur <jbrodeur001@gmail.com>
2021-10-21 18:55:21 -0400

    login & logout with session

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitiated: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
